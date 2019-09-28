c8bef869824547f1f000fbc66f97eb0a70f177da
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 06:01:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-  if self.valid? && @status == "pending"
+  if valid? && @status == "pending"
         @sender.withdraw(@amount)
         @receiver.deposit(@amount)
         @status = "complete"
