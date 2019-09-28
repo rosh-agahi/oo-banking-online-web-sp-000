2b8a040b670ab5f54ebb6be4348e576be340c820
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 05:53:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,6 @@ class Transfer
   def execute_transaction
   if @sender.balance < @amount
     return "Transaction rejected.Please check your account balance."
-    @status = "complete"
     else
       self.valid? && @status != "complete"
         @sender.withdraw(@amount)
