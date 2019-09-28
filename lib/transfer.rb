b51c58217362b0986c3a84e36ce058a591e0e821
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 05:46:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,9 @@ class Transfer
   
   def execute_transaction
   if self.valid?
-    @sender.balance
+    @sender.deposit(@amount)
+    @receiver.withdraw(@amount)
+  end
   end
 
   def reverse_transaction
